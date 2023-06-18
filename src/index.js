e887fac116ebf87f13a10b2409dcf2ef6e9866fc
Kento Nishi <kento24gs@outlook.com>
2023-06-18 17:08:41 -0400

    PROXY WORKS

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
 import WebTorrent from 'webtorrent'
-import fs from 'fs'
-import fetchMetadata from 'bep9-metadata-dl'
 import torrentEngine from 'torrent-stream'
 import LazyLoadChunkStore from './chunk-store.js'
 
-const infoHash = 'cd4712275bc68d895451977dab645d8874d353e5'
+const infoHash = '23f10a3383cce0a8f45480b24c838923cf84aff5'
 
 const client = new WebTorrent()
 const engine = torrentEngine(infoHash)
 
-const fetchMeta = async (arg) => {
-  const data = await fetchMetadata(arg, client.dht ? { dht: client.dht } : undefined)
-  // return {
-  //   name: data.info.name.toString(),
-  //   length: data.info.length,
-  //   pieceLength: data.info['piece length'],
-  //   pieces: data.info.pieces
-  // }
-  return data.info
-}
+engine.on('ready', async () => {
+  const meta = engine.torrent.info
+  const preloadedStore = new LazyLoadChunkStore(meta.length, meta['piece length'], engine)
 
-const meta = await fetchMeta(infoHash)
-const preloadedStore = new LazyLoadChunkStore(Math.ceil(meta.length / meta.pieceLength), engine)
-
-client.seed(null, {
-  metadata: meta,
-  infoHash,
-  preloadedStore
-}, (torrent) => {
-  console.log(`Torrent info hash: ${torrent.infoHash} ${torrent.magnetURI}`)
+  client.seed(null, {
+    metadata: meta,
+    infoHash,
+    preloadedStore
+  }, (torrent) => {
+    console.log(`Torrent info hash: ${torrent.infoHash} ${torrent.magnetURI}`)
+  })
 })
-
-// client.seed('/home/kento/github/wt-test/test.txt', {
-//   metadata: meta,
-//   infoHash
-// }, (torrent) => {
-//   console.log(`Torrent info hash: ${torrent.infoHash} ${torrent.magnetURI}`)
-//   // write torrent.torrentFile
-// })
