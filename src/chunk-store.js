741292f63f397bc2b395132f1b1e86e9d2f6780c
Kento Nishi <kento24gs@outlook.com>
2023-06-18 18:23:06 -0400

    some lru cache logic

--- src/chunk-store.js
+++ src/chunk-store.js
@@ -1,9 +1,25 @@
+import { LRUCache } from 'lru-cache'
+import { MAX_BYTES_SELECT_PER_TORRENT } from '../constants.js'
+
 class LazyLoadChunkStore {
   constructor (length, pieceLength, engine) {
     this.length = length
     this.pieceLength = pieceLength
     this.chunkLength = Math.ceil(length / pieceLength)
     this.engine = engine
+    this.lru = new LRUCache({
+      maxSize: MAX_BYTES_SELECT_PER_TORRENT,
+      sizeCalculation: (value, key) => {
+        return value.length
+      },
+      dispose: (value, key) => {
+        value.deselect()
+      }
+    })
+    engine.files.forEach((file, index) => {
+      file.select()
+      this.lru.set(index, file)
+    })
   }
 
   put (index, buf, cb) {
@@ -14,6 +30,7 @@ class LazyLoadChunkStore {
     if (typeof opts === 'function') return this.get(index, {}, opts)
     opts.offset = opts.offset || 0
     opts.length = opts.length || this.pieceLength
+    globalThis.engineLRU.get(this.engine.torrent.infoHash)
     const files = this.engine.torrent.files
     let fileIndex = 0
     let fileOffset = 0
@@ -26,13 +43,13 @@ class LazyLoadChunkStore {
       fileOffset += file.length
     }
     const file = this.engine.files[fileIndex]
+    this.lru.get(index)
     file.select()
     const stream = file.createReadStream({
       start: index * this.pieceLength - fileOffset + opts.offset,
       end: index * this.pieceLength - fileOffset + opts.offset + opts.length - 1
     })
     stream.on('data', (data) => {
-      console.log(`get ${index} ${opts.offset} ${opts.length}`, data)
       cb(null, data)
     })
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import WebTorrent from 'webtorrent'
 import torrentEngine from 'torrent-stream'
 import LazyLoadChunkStore from './chunk-store.js'
-
-const infoHash = '23f10a3383cce0a8f45480b24c838923cf84aff5'
+import { LRUCache } from 'lru-cache'
+import { MAX_NUM_PROXY } from '../constants.js'
 
 const client = new WebTorrent()
+globalThis.engineLRU = new LRUCache({
+  max: MAX_NUM_PROXY,
+  dispose: (key, value) => {
+    value.destroy()
+  }
+})
+
+const infoHash = 'e8bab7a37a347f30e85f932dcd15e4c0cc141aa8'
 const engine = torrentEngine(infoHash)
 
 engine.on('ready', async () => {
   const meta = engine.torrent.info
   const preloadedStore = new LazyLoadChunkStore(meta.length, meta['piece length'], engine)
-
+  globalThis.engineLRU.set(infoHash, engine)
   client.seed(null, {
     metadata: meta,
     infoHash,
     preloadedStore
   }, (torrent) => {
-    console.log(`Torrent info hash: ${torrent.infoHash} ${torrent.magnetURI}`)
+    console.log(`Proxying ${torrent.infoHash}`)
   })
 })
