e45f5189bf547f790941abe5f12af4685a5d776c
Kento Nishi <kento24gs@outlook.com>
2023-06-19 14:39:56 -0400

    set up coordinator

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-const */
 export let MAX_NUM_PROXY = 25
 export let MAX_BYTES_SELECT_PER_TORRENT = 1024 * 1024 * 1024
-export let COORDINATOR_URL = 'http://localhost:3000/listen'
+export let COORDINATOR_URL = 'https://coordinator.mantaray.tv/listen'
 
 export default {
   MAX_NUM_PROXY,
