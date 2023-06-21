3722ecc8041dd4da9c2d23c6d630939f9a608ae3
Kento Nishi <kento24gs@outlook.com>
2023-06-20 23:16:10 -0400

    just use every single tracker imaginable

--- src/index.js
+++ src/index.js
@@ -20,11 +20,33 @@ const engineLRU = new LRUCache({
 })
 
 const sse = new ReconnectingEventSource(C.COORDINATOR_URL)
+const trackers = Array.from(new Set([
+  ...(
+    (
+      await (
+        await fetch('https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt')
+      ).text()
+    ).split('\n').map((tracker) => tracker.trim()).filter((tracker) => tracker.length > 0)
+  ),
+  'udp://tracker.openbittorrent.com:80',
+  'udp://tracker.ccc.de:80',
+  'udp://tracker.internetwarriors.net:1337',
+  'udp://tracker.leechers-paradise.org:6969',
+  'udp://tracker.coppersurfer.tk:6969',
+  'udp://exodus.desync.com:6969',
+  'wss://tracker.btorrent.xyz',
+  'wss://tracker.openwebtorrent.com',
+  'wss://tracker.fastcast.nz',
+  'udp://tracker.opentrackr.org:1337',
+  'udp://explodie.org:6969',
+  'udp://tracker.empire-js.us:1337'
+]))
+globalThis.TRACKERS = trackers
 
 sse.addEventListener('message', async (event) => {
   const { infoHash } = JSON.parse(event.data)
   if (engineLRU.get(infoHash)) return
-  const engine = torrentEngine(infoHash)
+  const engine = torrentEngine(infoHash, { trackers })
   engine.on('ready', async () => {
     const meta = engine.torrent.info
     const preloadedStore = new LazyLoadChunkStore(meta.length, meta['piece length'], engine, engineLRU)
