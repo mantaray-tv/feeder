95e0bf45bd2ff9a497b7e2f31a04c56f127edcd3
Kento Nishi <kento24gs@outlook.com>
2023-08-21 20:07:01 +0000

    try theo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir -p /root/.cache/vlc
 iptables -A INPUT -p tcp --dport 6881:6889 -j DROP
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-vlc -I dummy /test.torrent --sout '#transcode{vcodec=theo,vb=5000,acodec=vorbis,soverlay}:std{access=http,mux=ogg,dst=:8080/stream.ogg}'
+vlc -I dummy /test.torrent --sout '#transcode{vcodec=theo,vb=5000,acodec=vorbis,soverlay}:std{access=http,mux=ogv,dst=:8080/stream.ogv}'
