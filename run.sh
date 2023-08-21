274f6551b2843dfddcbd597d6c7353bc31ec8e17
Kento Nishi <kento24gs@outlook.com>
2023-08-21 19:46:53 +0000

    try ogg?

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir -p /root/.cache/vlc
 iptables -A INPUT -p tcp --dport 6881:6889 -j DROP
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-vlc -I rc /test.torrent --sout '#transcode{vcodec=H264,threads=1,acodec=aac,soverlay}:std{access=http,mux=ts,dst=:8080/stream.m2ts}'
+vlc -I dummy /test.torrent --sout '#transcode{vcodec=theo,vb=5000,acodec=vorbis,soverlay}:std{access=http,mux=ogg,dst=:8080/stream.ogg}'
