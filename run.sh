5e04bf97b611c24553a205cea655117f9bca5e61
Kento Nishi <kento24gs@outlook.com>
2023-08-21 18:54:31 +0000

    h264 supremacy

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir -p /root/.cache/vlc
 iptables -A INPUT -p tcp --dport 6881:6889 -j DROP
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-vlc -I dummy /test.torrent --sout '#transcode{vcodec=VP80,vb=4000,acodec=vorb,soverlay}:std{access=http,mux=webm,dst=:8080/stream.webm}'
+vlc -I rc /test.torrent --sout '#transcode{vcodec=H264,threads=1,acodec=aac,soverlay}:std{access=http,mux=ts,dst=:8080/stream.m2ts}'
