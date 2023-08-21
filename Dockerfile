010bc2c1ef2974abbf5ad7740649fc5b8fd345ec
Kento Nishi <kento24gs@outlook.com>
2023-08-21 21:17:24 +0000

    try disabling hardware accl?

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ RUN apt update -y
 RUN apt install -y nodejs
 RUN apt install -y npm
 RUN apt install -y vlc
-RUN apt install -y iptables
 RUN apt install vlc-plugin-bittorrent -y
 COPY ./run.sh /run.sh
 COPY ./test.torrent /test.torrent
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 mkdir -p /root/.cache/vlc
-iptables -A INPUT -p tcp --dport 6881:6889 -j DROP
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-vlc -I dummy /test.torrent --sout '#transcode{vcodec=theo,vb=5000,acodec=vorbis,soverlay}:std{access=http,mux=ogv,dst=:8080/stream.ogv}'
+vlc -I telnet /test.torrent --sout '#transcode{vcodec=H264,threads=1,acodec=aac,soverlay}:std{access=http,mux=ts,dst=:8080/stream.ts}' --avcodec-hw=none
