337b1b1051ec3ef8455daaacba67880cfc284616
Kento Nishi <kento24gs@outlook.com>
2023-08-23 20:59:19 +0000

    m2ts

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir -p /root/.cache/vlc
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
 lcp --proxyUrl http://localhost:8000/ &
-vlc -I dummy /test.torrent vlc://quit --sout '#transcode{vcodec=h264,acodec=aac,soverlay,threads=1}:std{access=http,mux=ts,dst=:8000/stream.ts}'
+vlc -I dummy /test.torrent vlc://quit --sout '#transcode{vcodec=h264,acodec=aac,soverlay,threads=1}:std{access=http,mux=ts,dst=:8000/stream.m2ts}'
