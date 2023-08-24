f12d9824bf84cf72b1b47c3462d94546db0a48cc
Kento Nishi <kento24gs@outlook.com>
2023-08-24 02:15:06 +0000

    try not baking subs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir -p /root/.cache/vlc
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
 lcp --proxyUrl http://localhost:8000/ &
-vlc -I dummy /test.torrent vlc://quit --sout '#transcode{vcodec=h264,venc=x264{preset=ultrafast},acodec=mp3,soverlay}:std{access=http,mux=ts,dst=:8000/stream.ts}'  --avcodec-hw=none --no-audio
+vlc -I dummy /test.torrent vlc://quit --sout '#transcode{vcodec=h264,acodec=aac}:std{access=http,mux=ts,dst=:8000/stream.ts}'
