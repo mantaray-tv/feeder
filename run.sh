e3bbf2abe2d23dbb4e426bffb76d7b58cdd8de68
Kento Nishi <kento24gs@outlook.com>
2023-08-24 21:26:34 +0000

    try await

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ cd /live
 mkdir -p /root/.cache/vlc
 http-server -p 8443 --cors -c-1 > /dev/null &
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-sleep 1
 cpulimit -m -l 5 -- vlc -I dummy /test.torrent vlc://quit --sout '#http{mux=ts,dst=:8000/stream.ts}' &
-sleep 1
-while true; do
-cpulimit -m -l 5 -- ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
-sleep 1
+until $(curl --output /dev/null --silent --head --fail http://localhost:8000/stream.ts); do
+    printf '.'
+    sleep 1
 done
+sleep 3
+cpulimit -m -l 5 -- ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
