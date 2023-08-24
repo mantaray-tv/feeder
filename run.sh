391a7b954b379371985d8ffba8c22056df744d86
Kento Nishi <kento24gs@outlook.com>
2023-08-24 22:22:27 +0000

    transcode in vlc

--- run.sh
+++ run.sh
@@ -6,10 +6,9 @@ until $(curl --output /dev/null --silent --max-filesize 1024 --fail http://local
     sleep 1
 done
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-cpulimit -m -l 10 -- vlc -I dummy /test.torrent vlc://quit --sout '#http{mux=ts,dst=:8000/stream.ts}' &
+cpulimit -m -l 10 -- vlc -I dummy /test.torrent vlc://quit --sout '#transcode{vcodec=h264,acodec=aac,scodec=dvbs,venc=x264{preset=ultrafast}}:http{mux=ts,dst=:8000/stream.ts}' &
 until $(curl --output /dev/null --silent --max-filesize 1024 --fail http://localhost:8000/stream.ts); do
     printf '.'
     sleep 1
 done
-sleep 3
-cpulimit -m -l 10 -- ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
+cpulimit -m -l 5 -- ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8 &
