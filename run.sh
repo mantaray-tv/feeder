7c7105dab872d0adc87a82c7892c4717065267a9
Kento Nishi <kento24gs@outlook.com>
2023-08-24 21:50:16 +0000

    another build

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 cd /live
 mkdir -p /root/.cache/vlc
-http-server -p 8443 --cors -c-1 > /dev/null &
+http-server -p 8443 --cors -c-1 &
+until $(curl --output /dev/null --silent --max-filesize 1024 --fail http://localhost:8443); do
+    printf '.'
+    sleep 1
+done
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-cpulimit -m -l 5 -- vlc -I dummy /test.torrent vlc://quit --sout '#http{mux=ts,dst=:8000/stream.ts}' &
-until $(curl --output /dev/null --silent --head --fail http://localhost:8000/stream.ts); do
+cpulimit -m -l 10 -- vlc -I dummy /test.torrent vlc://quit --sout '#http{mux=ts,dst=:8000/stream.ts}' &
+until $(curl --output /dev/null --silent --max-filesize 1024 --fail http://localhost:8000/stream.ts); do
     printf '.'
     sleep 1
 done
 sleep 3
-cpulimit -m -l 5 -- ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
+cpulimit -m -l 10 -- ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
