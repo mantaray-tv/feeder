ee334e030338262a35faf6bffa7d2be0eb43dffc
Kento Nishi <kento24gs@outlook.com>
2023-08-25 05:41:34 +0000

    max 8 conns

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,8 @@ RUN apt install -y npm
 RUN apt install -y vlc
 RUN apt install vlc-plugin-bittorrent -y
 RUN apt install ffmpeg -y
-RUN apt install cpulimit -y
-RUN apt install -y curl
-RUN apt install -y iproute2
-RUN apt install -y iptables
 RUN npm install -g http-server
+RUN npm install -g peerflix
 RUN mkdir /live
 COPY ./index.html /live/index.html
 COPY ./run.sh /run.sh
--- run.sh
+++ run.sh
@@ -1,14 +1,8 @@
 cd /live
 mkdir -p /root/.cache/vlc
 http-server -p 8443 --cors -c-1 &
-iptables -A INPUT -p tcp --destination-port 6881:6889 -j DROP
-tc qdisc add dev eth0 root tbf rate 5kbps latency 50ms burst 1540
-until $(curl --output /dev/null --silent --max-filesize 1024 --fail http://localhost:8443); do
-    echo Waiting for server
-    sleep 1
-done
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-vlc -I dummy /test.torrent vlc://quit --sout '#http{mux=ts,dst=:8000/stream.ts}' &
+peerflix /test.torrent --connections 8 --vlc -- -I dummy --sout '#http{mux=ts,dst=:8000/stream.ts}' &
 while true; do
     echo "Waiting for stream"
     ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
