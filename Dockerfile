840877d3e8be73a97b6d6c4b20a6f1a78d8dbd7e
Kento Nishi <kento24gs@outlook.com>
2023-08-25 02:06:56 +0000

    iptables image

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,8 @@ RUN apt install vlc-plugin-bittorrent -y
 RUN apt install ffmpeg -y
 RUN apt install cpulimit -y
 RUN apt install -y curl
+RUN apt install -y iproute2
+RUN apt install -y iptables
 RUN npm install -g http-server
 RUN mkdir /live
 COPY ./index.html /live/index.html
--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 cd /live
 mkdir -p /root/.cache/vlc
 http-server -p 8443 --cors -c-1 &
+iptables -A INPUT -p tcp --destination-port 6881:6889 -j DROP
 until $(curl --output /dev/null --silent --max-filesize 1024 --fail http://localhost:8443); do
-    printf '.'
+    echo Waiting for server
     sleep 1
 done
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-cpulimit -m -l 10 -- vlc -I dummy /test.torrent vlc://quit --sout '#transcode{vcodec=h264,acodec=aac,scodec=dvbs,venc=x264{preset=ultrafast}}:http{mux=ts,dst=:8000/stream.ts}' &
-until $(curl --output /dev/null --silent --max-filesize 1024 --fail http://localhost:8000/stream.ts); do
-    printf '.'
-    sleep 1
+vlc -I dummy /test.torrent vlc://quit --sout '#http{mux=ts,dst=:8000/stream.ts}' &
+while true; do
+    echo "Waiting for stream"
+    ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
+    if [ $? -eq 0 ]; then
+        break
+    fi
 done
-cpulimit -m -l 5 -- ffmpeg -i http://localhost:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8 &
