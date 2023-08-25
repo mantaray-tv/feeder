2db7ab794b67d42747a6886b4855a5d2d9b851cf
Kento Nishi <kento24gs@outlook.com>
2023-08-25 21:20:52 +0000

    webm again?

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,14 @@ RUN apt update -y
 RUN apt install -y nodejs
 RUN apt install -y npm
 RUN apt install -y vlc
-RUN apt install vlc-plugin-bittorrent -y
+# RUN apt install vlc-plugin-bittorrent -y
 RUN apt install ffmpeg -y
 RUN apt install -y tor
 RUN apt install -y torsocks
 RUN apt install -y curl
-RUN npm install -g http-server
+# RUN npm install -g http-server
+RUN npm install -g peerflix
+RUN npm install -g local-cors-proxy
 RUN mkdir /live
 COPY ./index.html /live/index.html
 COPY ./run.sh /run.sh
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 cd /live
 mkdir -p /root/.cache/vlc
-http-server -p 8443 --cors -c-1 &
+# http-server -p 8443 --cors -c-1 &
+lcp --proxyUrl http://localhost:8000/ --port 8443 &
 tor &
 while true; do
     echo "Waiting for tor to start"
@@ -10,11 +11,12 @@ while true; do
     fi
 done
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
-torsocks vlc -I dummy /test.torrent vlc://quit --sout '#transcode{vcodec=h264,scale=Auto,acodec=aac,scodec=dvbs}:std{access=http,mux=ts,dst=:8000/stream.ts}' &
-while true; do
-    echo "Waiting for stream"
-    ffmpeg -i http://127.0.0.1:8000/stream.ts -c copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
-    if [ $? -eq 0 ]; then
-        break
-    fi
-done
+torsocks peerflix /test.torrent --vlc -- -I dummy vlc://quit --sout "'#transcode{vcodec=VP80,acodec=vorb,soverlay}:std{access=http,mux=webm,dst=:8000/stream.webm}'" &
+# torsocks peerflix /test.torrent vlc -I dummy vlc://quit --sout '#std{access=http,mux=ts,dst=:8000/stream.ts}' &
+# while true; do
+#     echo "Waiting for stream"
+#     ffmpeg -i http://127.0.0.1:8000/stream.ts -c copy -c:v libx264 -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
+#     if [ $? -eq 0 ]; then
+#         break
+#     fi
+# done
