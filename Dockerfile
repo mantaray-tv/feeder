171cc49379bf2a5aa01e6fa68f712791f47e47ef
Kento Nishi <kento24gs@outlook.com>
2023-08-26 06:32:10 +0000

    back to vlc?

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM ubuntu:23.04
 LABEL org.opencontainers.image.source=https://github.com/mantaray-tv/feeder
 
 RUN apt update -y
-# RUN apt install vlc-plugin-bittorrent -y
+RUN apt install vlc-plugin-bittorrent -y
 RUN apt install -y tor
 # RUN apt install -y git
 RUN apt install -y nodejs
@@ -26,7 +26,7 @@ RUN apt install -y curl
 RUN apt install -y vlc
 RUN npm install -g http-server
 # RUN npm install -g peerflix
-RUN npm install -g local-cors-proxy
+# RUN npm install -g local-cors-proxy
 RUN mkdir /live
 COPY ./index.html /live/index.html
 COPY ./run.sh /run.sh
--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ http-server -p 8443 --cors -c-1 &
 tor &
 while true; do
     echo "Waiting for tor to start"
-    (npm list -g peerflix || npm i -g peerflix) && curl --socks5-hostname localhost:9050 -s https://check.torproject.org/ | grep -m 1 Congratulations
+    # (npm list -g peerflix || npm i -g peerflix) && 
+    curl --socks5-hostname localhost:9050 -s https://check.torproject.org/ | grep -m 1 Congratulations
     if [ $? -eq 0 ]; then
         break
     fi
@@ -14,7 +15,8 @@ done
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
 # torsocks peerflix /test.torrent --vlc -- -I dummy vlc://quit --sout "'#transcode{vcodec=VP80,acodec=vorb,soverlay}:std{access=http,mux=webm,dst=:8000/stream.webm}'" &
 # torsocks peerflix /test.torrent --vlc -- -I dummy --sout "'#transcode{vcodec=h264,acodec=aac,soverlay}:std{access=http,mux=ts,dst=:8000/stream.ts}'" &
-torsocks peerflix $1 --vlc -- -I dummy --sout "'#transcode{vcodec=h264,acodec=aac,soverlay,vb=6000,ab=320}:std{access=http,mux=ts,dst=:8000/stream.ts}'" &
+# torsocks peerflix $1 --vlc -- -I dummy --sout "'#transcode{vcodec=h264,acodec=aac,soverlay,vb=6000,ab=320}:std{access=http,mux=ts,dst=:8000/stream.ts}'" &
+vlc -I dummy --sout '#transcode{vcodec=h264,acodec=aac,soverlay,vb=6000,ab=320}:std{access=http,mux=ts,dst=:8000/stream.ts}' &
 while true; do
     echo "Waiting for stream"
     ffmpeg -i http://127.0.0.1:8000/stream.ts -c:v copy -c:a copy -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
