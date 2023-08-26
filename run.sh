fdee43d4494a3cce6d11d94aef4474609432db8f
Kento Nishi <kento24gs@outlook.com>
2023-08-26 02:06:57 +0000

    sleep *after*

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ http-server -p 8443 --cors -c-1 &
 tor &
 while true; do
     echo "Waiting for tor to start"
-    sleep 1
     curl --socks5-hostname localhost:9050 -s https://check.torproject.org/ | grep -m 1 Congratulations
     if [ $? -eq 0 ]; then
         break
     fi
+    sleep 1
 done
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
 # torsocks peerflix /test.torrent --vlc -- -I dummy vlc://quit --sout "'#transcode{vcodec=VP80,acodec=vorb,soverlay}:std{access=http,mux=webm,dst=:8000/stream.webm}'" &
@@ -18,8 +18,8 @@ torsocks vlc /test.torrent vlc://quit -I dummy --sout '#transcode{vcodec=h264,ac
 while true; do
     echo "Waiting for stream"
     ffmpeg -i http://127.0.0.1:8000/stream.ts -c:v copy -c:a copy -hls_time 10 -hls_list_size 10 -segment_wrap 10 -hls_flags delete_segments -hls_segment_filename "/live/output_%03d.ts" /live/stream.m3u8
-    sleep 1
     if [ $? -eq 0 ]; then
         break
     fi
+    sleep 1
 done
